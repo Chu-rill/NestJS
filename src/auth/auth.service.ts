d4ceba0278f8b9eec756fe522ca4f98b3ee7d263
Chu-rill <churchillrimamskep@gmail.com>
2024-12-10 22:47:17 +0100

    added more to the code base

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  signup() {
+    return { msg: "I have singup" };
+  }
+  login() {
+    return { msg: "I have login" };
+  }
+}
